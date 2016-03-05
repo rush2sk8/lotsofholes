80c8f5c6c94b0ba5035a4a662b834033813b997c
Rushad Antia <nope>
2016-03-05 18:13:14 -0500

    setup location

--- src/com/example/lotsofholes/MainActivity.java
+++ src/com/example/lotsofholes/MainActivity.java
@@ -2,20 +2,28 @@ package com.example.lotsofholes;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements SensorEventListener{
+public class MainActivity extends Activity implements SensorEventListener, LocationListener{
 
     private SensorManager sensorManager;
+    private LocationManager locationManager;
     private Sensor linAcc, acc;
     private TextView linAccTv , accTv;
-
+    private int lon , lat;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +33,17 @@ public class MainActivity extends Activity implements SensorEventListener{
 
 	linAccTv = (TextView)findViewById(R.id.linacc);
 	accTv = (TextView)findViewById(R.id.acc);
-	
+
 	sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 	acc = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 	linAcc = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+	locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+	if (!enabled) 
+	    startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+
 
     }
 
@@ -39,10 +54,10 @@ public class MainActivity extends Activity implements SensorEventListener{
 
 	if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) 
 	    filterAccValues(event);
-	
+
 	else if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION ) 
 	    filterLinAccValues(event);
-	
+
 
     }
 
@@ -50,40 +65,68 @@ public class MainActivity extends Activity implements SensorEventListener{
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-	
-
-    }
 
 
-    @Override
-    protected void onPause() {
-	sensorManager.unregisterListener(this);
-
-	super.onPause();
-    }
-
-    @Override
-    protected void onResume() {
-	sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
-	sensorManager.registerListener(this, linAcc, SensorManager.SENSOR_DELAY_NORMAL);
-	super.onResume();
     }
 
     private void filterAccValues(SensorEvent event) {
 	float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-        accTv.setText("X: "+ x+" Y: "+ y+ " Z: "+ z);
-	
+	float y = event.values[1];
+	float z = event.values[2];
+	accTv.setText("X: "+ x+" Y: "+ y+ " Z: "+ z);
+
     }
-    
+
     private void filterLinAccValues(SensorEvent event) {
 	float x = event.values[0];
-        float y = event.values[1];
-        float z = event.values[2];
-        
-        linAccTv.setText("X: "+ x+" Y: "+ y+ " Z: "+ z);
-        
+	float y = event.values[1];
+	float z = event.values[2];
+
+	linAccTv.setText("X: "+ x+" Y: "+ y+ " Z: "+ z);
+
     }
-    
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+	lat = (int) (location.getLatitude());
+	lon = (int) (location.getLongitude());
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+	Toast.makeText(this, "Enabled new provider " + provider,
+		Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+	Toast.makeText(this, "Disabled provider " + provider,
+		Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+	super.onPause();
+	sensorManager.unregisterListener(this);
+	locationManager.removeUpdates(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+	super.onResume();
+	sensorManager.registerListener(this, acc, SensorManager.SENSOR_DELAY_NORMAL);
+	sensorManager.registerListener(this, linAcc, SensorManager.SENSOR_DELAY_NORMAL);
+	locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
 }
